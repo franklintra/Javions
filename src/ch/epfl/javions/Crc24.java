3dec43e54c955766c417d443a330deb3f53f8b31
Krish Chawla <krishchawla112@gmail.com>
2023-02-26 00:40:28 +0100

    temporary push, CRC24 works without buildTable

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -4,18 +4,35 @@ package ch.epfl.javions;
  * @author @franklintra
  * @project Javions
  */
-public class Crc24 {
-    public final static int GENERATOR = 0xFFF409;
-    private final static int[] TABLE = buildTable();
+public final class Crc24 {
+    public static final int GENERATOR = 0xFFF409;
+    public final int generator;
+    //private final int[] TABLE;
+
 
     public Crc24(int generator) {
+        this.generator = generator;
+//        TABLE = buildTable(generator);
     }
 
-    public static int crc(byte[] bytes) {
-        return crc_bitwise(bytes, GENERATOR);
+//    public int crc(byte[] bytes) {
+//        return crc_bitwise(bytes, generator);
+//    }
+public int crc(byte[] bytes) {
+    int crc = 0;
+    for (byte b : bytes) {
+        crc ^= (b & 0xff) << 16;
+        for (int i = 0; i < 8; i++) {
+            crc <<= 1;
+            if ((crc & 0x1000000) != 0) {
+                crc ^= GENERATOR;
+            }
+        }
     }
+    return crc & 0xffffff;
+}
 
-    private static int crc_bitwise(byte[] bytes, int generator) {
+    private int crc_bitwise(byte[] bytes, int generator) {
         int crc = 0;
         for (byte b : bytes) {
             crc  = (crc << 1) | b;
@@ -30,19 +47,20 @@ public class Crc24 {
     /**
      * Builds a table of 256 24-bit CRCs of all possible 8-bit bytes.
      */
-    private static int[] buildTable() {
-        int[] table = new int[256];
-        for (int n = 0; n < 256; n++) {
-            int c = n << 16;
-            for (int k = 0; k < 8; k++) {
-                if ((c & 0x800000) != 0) {
-                    c = (c << 1) ^ GENERATOR;
-                } else {
-                    c <<= 1;
-                }
-            }
-            table[n] = c;
-        }
-        return table;
-    }
+//    private int[] buildTable() {
+//        int[] table = new int[256];
+//        for (int n = 0; n < 256; n++) {
+//            int c = n << 16;
+//            for (int k = 0; k < 8; k++) {
+//                if ((c & 0x800000) != 0) {
+//                    c = (c << 1) ^ generator;
+//                } else {
+//                    c <<= 1;
+//                }
+//            }
+//            table[n] = c;
+//        }
+//        return table;
+//    }
+
 }
--- test/ch/epfl/javions/Crc24Test.java
+++ test/ch/epfl/javions/Crc24Test.java
@@ -17,7 +17,8 @@ class Crc24Test {
      */
     @Test
     void crc() {
-        String[] messages = new String[]{"8D392AE499107FB5C00439", "8D3950C69914B232880436", "8D4B17E399893E15C09C21", "8D4B18F4231445F2DB63A0", "8D495293F82300020049B8"};
+        Crc24 calculator = new Crc24(Crc24.GENERATOR);
+        String[] messages = new String[]{"8D392AE499107FB5C00439", "8D4D2286EA428867291C08", "8D3950C69914B232880436", "8D4B17E399893E15C09C21", "8D4B18F4231445F2DB63A0", "8D495293F82300020049B8"};
         String[] expectedCrcs = new String[]{"035DB8", "EE2EC6", "BC63D3", "9FC014", "DEEB82", "111203"};
         int c;
         byte[] mAndC;
@@ -26,10 +27,10 @@ class Crc24Test {
         for (int i = 0; i < messages.length; i++){
             c = Integer.parseInt(expectedCrcs[i], 16);
             mAndC = HexFormat.of().parseHex(messages[i] + expectedCrcs[i]);
-            assertEquals(0, Crc24.crc(mAndC));
+            assertEquals(0, calculator.crc(mAndC));
 
             mONly = HexFormat.of().parseHex(messages[i]);
-            assertEquals(c, Crc24.crc(mONly));
+            assertEquals(c, calculator.crc(mONly));
             System.out.println(i+1+"/"+messages.length+"Testing message: " + messages[i] + " with expected CRC: " + expectedCrcs[i]);
         }
     }
