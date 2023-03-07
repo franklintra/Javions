258834443224725a91c60d4c9f66eb616308feb6
Krish Chawla <krishchawla112@gmail.com>
2023-03-07 13:22:10 +0100

    got rid of second Preconditions method

--- src/ch/epfl/javions/Preconditions.java
+++ src/ch/epfl/javions/Preconditions.java
@@ -18,13 +18,4 @@ public class Preconditions {
         }
     }
 
-    /**
-     * @param shouldBeTrue the condition to check
-     * @throws IllegalArgumentException if the condition is not met
-     */
-    public static void checkArgument(boolean shouldBeTrue, String message) throws IllegalArgumentException {
-        if (!shouldBeTrue) {
-            throw new IllegalArgumentException(message);
-        }
-    }
 }
