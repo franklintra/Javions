ccea98805d337865e961463a45c438efd5e61e7f
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-07 13:16:39 +0100

    Renamed test wrongfully named

--- test/ch/epfl/javions/demodulation/ClassPowerWindowTest.java
+++
@@ -1,7 +0,0 @@
-package ch.epfl.javions.demodulation;
-
-import org.junit.jupiter.api.Test;
-
-public class ClassPowerWindowTest {
-
-}
---
+++ test/ch/epfl/javions/demodulation/PowerWindowTest.java
@@ -0,0 +1,5 @@
+package ch.epfl.javions.demodulation;
+
+public class PowerWindowTest {
+
+}
