42ba0c45fb9985950018053754206fa800aa85c3
Krish Chawla <krishchawla112@gmail.com>
2023-03-14 13:04:55 +0100

    finished RawMessage, not sure about two things in class

--- src/ch/epfl/javions/adsb/RawMessage.java
+++ src/ch/epfl/javions/adsb/RawMessage.java
@@ -16,37 +16,78 @@ public record RawMessage(long timeStampNs, ByteString bytes) {
         Preconditions.checkArgument(timeStampNs >= 0 && bytes.size() == LENGTH);
     }
 
+    /**
+     * Returns a RawMessage object if the CRC24 of the bytes is 0, null otherwise.
+     *
+     * @param timeStampNs the time stamp in nanoseconds
+     * @param bytes       the 14 bytes of the message
+     * @return a RawMessage object if the CRC24 of the bytes is 0, null otherwise
+     */
+    public
     static RawMessage of(long timeStampNs, byte[] bytes) {
-        Crc24 crc = new Crc24(Crc24.GENERATOR); // TODO: 3/13/2023 check generator
+        Crc24 crc = new Crc24(Crc24.GENERATOR); // FIXME: 3/14/2023 check if this is correct and generator
         return crc.crc(bytes) != 0 ? null : new RawMessage(timeStampNs, new ByteString(bytes));
-        // return raw ADS B message with given timestamp and bytes
+
     }
 
+    /**
+     * Returns valid length of message if DF is worth 17, otherwise null.
+     *
+     * @param byte0 the first byte of the message
+     * @return the valid length of message if DF is worth 17, otherwise null.
+     */
     static int size(byte byte0) {
             int df = byte0 & 0b11111000;
             return df == 0b10001000 ? LENGTH : 0;
     }
 
-    static int typeCode(long payload) { // TODO: 3/13/2023
-        // extract the type code from the ME attribute
-        long me = (payload >> 32) & 0x00FFFFFF80L;
-        return (int) (me >> 8);
-        //return (int) (payload >>> 32) & 0x1f;
+    /**
+     * Returns the type code of the message.
+     *
+     * @param payload the payload of the message
+     * @return the type code of the message
+     */
+    static int typeCode(long payload) { // FIXME: 3/14/2023 check if this is correct
+        // Bit mask to extract the 5 MSB of a 56-bit long integer
+        long msbMask = 0b11111_000000000000000000000000000000000000000000000000000L;
+        // Apply the bit mask to the payload and shift the result to the right
+        // by 51 bits to obtain the 5 MSB as an integer value
+        return (int) ((payload & msbMask) >>> 51);
     }
 
-    public int downlinkFormat() {
+    /**
+     * Returns the downlink format of the message.
+     *
+     * @return the downlink format of the message
+     */
+    public int downLinkFormat() {
         return  bytes.byteAt(0) & 0b11111000;
     }
 
-    public IcaoAddress icaoAddress() {
+    /**
+     * Returns the ICAO address of the message.
+     *
+     * @return the ICAO address of the message
+     */
+    public IcaoAddress IcaoAddress() {
     return new IcaoAddress(Long.toString(bytes.bytesInRange(1,3)));
     }
 
+    /**
+     * Returns the payload of the message.
+     *
+     * @return the payload of the message
+     */
     public long payload() {
         return bytes.bytesInRange(4, 10);
     }
 
-    public int typeCode() { // TODO: 3/13/2023
+    /**
+     * Returns the type code of the message.
+     *
+     * @return the type code of the message
+     */
+    public int typeCode() {
         return typeCode(payload());
     }
 
--- test/ch/epfl/javions/adsb/RawMessageTest.java
+++ test/ch/epfl/javions/adsb/RawMessageTest.java
@@ -1,8 +1,12 @@
 package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.ByteString;
+import ch.epfl.javions.Crc24;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -21,6 +25,16 @@ public class RawMessageTest {
         assertThrows(IllegalArgumentException.class, () -> new RawMessage(1, new ByteString(new byte[13])));
     }
 
+    @Test
+    void ofReturnsNullIfCRCNotZero() { // TODO: 3/14/2023 doesnt work
+        byte[] bytes = new byte[14];
+        Crc24 crc = new Crc24(Crc24.GENERATOR);
+
+        if (crc.crc(bytes) != 0) {
+            assertThrows(null, () -> RawMessage.of(1, bytes));
+        }
+
+    }
 
 
 }
