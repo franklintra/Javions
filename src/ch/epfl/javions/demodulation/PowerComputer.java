d4e284691b99542d3d5108d81c7eea07d12cf72c
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-15 19:09:36 +0100

    Finished PowerComputer.java
    Took me a long time to add a very simple boolean variable to know if we have to generate the last8Samples from the beginning!! Read the instruction set more thoroughly next time.

--- src/ch/epfl/javions/demodulation/PowerComputer.java
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -4,7 +4,6 @@ import ch.epfl.javions.Preconditions;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
 /**
  * @author @franklintra
@@ -18,6 +17,7 @@ public final class PowerComputer {
     private final short[] sampleBuffer; // this is the buffer that will contain the samples read from the input stream.
     private final int[] last8Samples = new int[8]; // this is the buffer that will contain the last 8 samples used to calculate the power
     private final int batchSize;
+    private boolean isWriting = false;
 
     public PowerComputer(InputStream stream, int batchSize) {
         Preconditions.checkArgument(batchSize > 0 && batchSize % 8 == 0);
@@ -32,7 +32,7 @@ public final class PowerComputer {
      * @return the modulus of the number base 8
      */
     private int base8Mod(int index) {
-        return Math.floorMod(index, 8); // this is used so that it works with negative values (the % operator in java gives the remainder instead of the modulus)
+        return Math.floorMod(index, 8);// this is used so that it works with negative values (the % operator in java gives the remainder instead of the modulus)
     }
 
     /**
@@ -44,28 +44,28 @@ public final class PowerComputer {
      */
     public int readBatch(int[] batch) throws IOException {
         Preconditions.checkArgument(batch.length == batchSize);
-        int bufferIndex = 2;
+        if (!isWriting) {
+            decoder.readBatch(sampleBuffer); //read first batch of samples
+            last8Samples[0] = sampleBuffer[0];
+            last8Samples[1] = sampleBuffer[1];
+        }
         int written = 0;
-        int read = decoder.readBatch(sampleBuffer); //read first batch of samples
-        //put latest data in last8Samples
-        last8Samples[6] = sampleBuffer[0];
-        last8Samples[7] = sampleBuffer[1];
+        int bufferIndex = 2;
 
-        for (int i = 2; i < batchSize+batchSize+2; i+=2) {
-            if (Math.floorMod(i, batchSize) == 0) {
-                read += decoder.readBatch(sampleBuffer); //decode new data only if needed
-                bufferIndex = 0;
+        for (int i = 0; i < batchSize ; i += 1) {
+            if (Math.floorMod(bufferIndex, batchSize) == 0) {
+                decoder.readBatch(sampleBuffer); //decode new data only if needed
             }
-            int evenIndexes = last8Samples[base8Mod((bufferIndex - 2) - 6)] - last8Samples[base8Mod((bufferIndex - 2) - 4)] + last8Samples[base8Mod((bufferIndex - 2) - 2)] - last8Samples[base8Mod((bufferIndex - 2))];
-            int oddIndexes = last8Samples[base8Mod((bufferIndex - 2) - 5)] - last8Samples[base8Mod((bufferIndex - 2) - 3)] + last8Samples[base8Mod((bufferIndex - 2) - 1)] - last8Samples[base8Mod((bufferIndex - 2) + 1)];
-            batch[i / 2 - 1] = evenIndexes * evenIndexes + oddIndexes * oddIndexes;
-
+            int evenIndexes = last8Samples[base8Mod(bufferIndex - 6)] - last8Samples[base8Mod(bufferIndex - 4)] + last8Samples[base8Mod(bufferIndex - 2)] - last8Samples[base8Mod((bufferIndex))];
+            int oddIndexes = last8Samples[base8Mod(bufferIndex - 5)] - last8Samples[base8Mod(bufferIndex - 3)] + last8Samples[base8Mod(bufferIndex - 1)] - last8Samples[base8Mod((bufferIndex + 1))];
+            batch[i] = evenIndexes * evenIndexes + oddIndexes * oddIndexes;
             // turnover latest data in last8Samples
             last8Samples[base8Mod(bufferIndex)] = sampleBuffer[bufferIndex];
-            last8Samples[base8Mod((bufferIndex) + 1)] = sampleBuffer[bufferIndex + 1];
-            bufferIndex = Math.floorMod(bufferIndex, sampleBuffer.length - 2);
+            last8Samples[base8Mod(bufferIndex + 1)] = sampleBuffer[bufferIndex + 1];
+            bufferIndex = Math.floorMod(bufferIndex + 2, batchSize);
             written++;
         }
+        isWriting = true;
         return written;
     }
 }
