d1aa2ff1a49809ca1f7ecee310f4091e16926be6
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-16 18:13:36 +0100

    progress do the fixme

--- src/ch/epfl/javions/adsb/AdsbDemodulator.java
+++ src/ch/epfl/javions/adsb/AdsbDemodulator.java
@@ -9,7 +9,7 @@ import java.util.HexFormat;
 
 public final class AdsbDemodulator {
 
-    private static int windowSize = 1200;
+    private final static int windowSize = 1200;
     private final InputStream samplesStream; // the stream of samples to be demodulated
     private final PowerWindow powerWindow; // the power window used to demodulate the ADS-B messages
 
@@ -31,6 +31,18 @@ public final class AdsbDemodulator {
         return null;
     }
 
+    private boolean getBitAt(long position) {
+        return false; // todo: return the bit at a position. to be used in the next method
+    }
+
+    private byte[] getBytes() {
+        return null; // todo: give the bytes from the current window
+    }
+
+    private int sumAt(int i) {
+        return powerWindow.get(i)+powerWindow.get(10 + i)+powerWindow.get(35 + i)+powerWindow.get(45 + i);
+    }
+
     /**
      * This method returns the next ADS-B message in the stream.
      * @return the next ADS-B message in the stream
@@ -38,24 +50,19 @@ public final class AdsbDemodulator {
      */
     public RawMessage nextMessage() throws IOException {
         RawMessage message = null;
-        int[] sums = new int[3];
-        int index = -1;
-        while (true) {
-            index++;
-            sums[index % 3] = powerWindow.get(0)+powerWindow.get(10)+powerWindow.get(35)+powerWindow.get(45);
-            if (sums[index%3] <= sums[(index+1)%3] || sums[index%3] <= sums[(index+2)%3]) {
-                powerWindow.advanceBy(windowSize);
-                continue;
+        while (powerWindow.isFull()) {
+            if (sumAt(0) >= sumAt(-1) && sumAt(0) >= sumAt(1)) { //  superieur a +1 et -1
+                int ev = powerWindow.get(5) + powerWindow.get(15) + powerWindow.get(20) + powerWindow.get(25) + powerWindow.get(30) + powerWindow.get(40);
+                if ( sumAt(0) >= 2*ev) {
+                    if (false) { // todo : check if the df is 17
+                        //decodage
+                        // decode and return everything
+                        powerWindow.advanceBy(windowSize);
+                    }
+                }
             }
-            if (sums[index] > 2*(powerWindow.get(5)+powerWindow.get(15)+ powerWindow.get(20)+powerWindow.get(25)+powerWindow.get(30)+powerWindow.get(40))) {
-                powerWindow.advanceBy(2); // this is to go forward 1/4 microsecond
-                message = new RawMessage(1L, getBits());
-                powerWindow.advanceBy(10); // fixme : advance by message bits (i don't remember how long it is)
-                return message;
-            }
-            else {
-                powerWindow.advance();
-            }
+            powerWindow.advance();
         }
+        return null;
     }
 }
