113e4e93455539c36450bf4a93248f089fc78708
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-17 16:18:58 +0100

    All is fixed!

--- test/ch/epfl/javions/adsb/AdsbDemodulatorTest.java
+++ test/ch/epfl/javions/adsb/AdsbDemodulatorTest.java
@@ -54,21 +54,19 @@ class AdsbDemodulatorTest {
         String[] expectedFive = new String[]{"RawMessage[timeStampNs=8096200, bytes=8D4B17E5F8210002004BB8B1F1AC]", "RawMessage[timeStampNs=75898000, bytes=8D49529958B302E6E15FA352306B]", "RawMessage[timeStampNs=100775400, bytes=8D39D300990CE72C70089058AD77]", "RawMessage[timeStampNs=116538700, bytes=8D4241A9601B32DA4367C4C3965E]", "RawMessage[timeStampNs=129268900, bytes=8D4B1A00EA0DC89E8F7C0857D5F5]"};
         String[] firstFive = new String[5];
         String f = "samples_20230304_1442.bin";
-        int counter = 5;
+        int messageAmount = 0;
         try (InputStream s = getClass().getResourceAsStream("/samples_20230304_1442.bin")) {
             AdsbDemodulator d = new AdsbDemodulator(s);
             RawMessage m;
-            for (int i = 0; i < 5; i++) {
-                firstFive[i] = d.nextMessage().toString();
-            }
             while ((m = d.nextMessage()) != null) {
-                counter++;
-                //System.out.println(m);
+                if (messageAmount < 5) {
+                    firstFive[messageAmount] = m.toString();
+                }
+                messageAmount++;
             }
         }
         for (int i = 0; i < 5; i++) {
             assertEquals(expectedFive[i], firstFive[i]);
         }
-        System.out.println("nombre de message =" + counter);
     }
 }
