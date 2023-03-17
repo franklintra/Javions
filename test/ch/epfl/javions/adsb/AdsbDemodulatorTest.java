d43db66e58f5bb9fb8f059f0d799a2dfef3d401e
Krish Chawla <krish.chawla@epfl.ch>
2023-03-17 16:48:12 +0100

    test fixed for IOException readMessage

--- test/ch/epfl/javions/adsb/AdsbDemodulatorTest.java
+++ test/ch/epfl/javions/adsb/AdsbDemodulatorTest.java
@@ -38,6 +38,16 @@ class AdsbDemodulatorTest {
     }
 
     @Test
+    public void testNextMessageWithNullInput() throws IOException {
+        assertThrows(IOException.class, () -> {
+            AdsbDemodulator adsbDemodulator = new AdsbDemodulator(new FileInputStream("invalidFile.txt"));
+            new AdsbDemodulator(new FileInputStream("invalidFile.txt"));
+        });
+    }
+
+
+
+    @Test
     void testFirstFiveMessages() throws IOException {
         String[] expectedFive = new String[]{"RawMessage[timeStampNs=8096200, bytes=8D4B17E5F8210002004BB8B1F1AC]", "RawMessage[timeStampNs=75898000, bytes=8D49529958B302E6E15FA352306B]", "RawMessage[timeStampNs=100775400, bytes=8D39D300990CE72C70089058AD77]", "RawMessage[timeStampNs=116538700, bytes=8D4241A9601B32DA4367C4C3965E]", "RawMessage[timeStampNs=129268900, bytes=8D4B1A00EA0DC89E8F7C0857D5F5]"};
         String[] firstFive = new String[5];
