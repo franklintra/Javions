3fcbfa67bd86033d843d611189dff7eb5107cac2
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-22 21:44:02 +0100

    Made empty test class + updated slightly Submit to better hide our tokens

---
+++ test/ch/epfl/javions/adsb/AirbornePositionMessageTest.java
@@ -0,0 +1,12 @@
+package ch.epfl.javions.adsb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author @franklintra
+ * @project Javions
+ */
+@SuppressWarnings("unused")
+class AirbornePositionMessageTest {
+
+}
