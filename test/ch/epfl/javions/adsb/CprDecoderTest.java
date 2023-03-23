69ab00455efd32fbe04633b77991e0f5fc4337d6
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-23 16:43:43 +0100

    CprDecoder works fine :)

--- test/ch/epfl/javions/adsb/CprDecoderTest.java
+++ test/ch/epfl/javions/adsb/CprDecoderTest.java
@@ -1,7 +1,9 @@
 package ch.epfl.javions.adsb;
 
+import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.Units;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
@@ -9,6 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @project Javions
  */
 class CprDecoderTest {
+
+    int[] long_CPR = new int[] {111600, 108865};
+    int[] lat_CPR = new int[] {94445, 77558};
+
+    double[] normalizedLong_CPR = new double[] {long_CPR[0]*Math.pow(2, -17), long_CPR[1]*Math.pow(2, -17)};
+    double[] normalizedLat_CPR = new double[] {lat_CPR[0]*Math.pow(2, -17), lat_CPR[1]*Math.pow(2, -17)};
+
     @Test
     void throwExceptionIfMostRecentIsNot0Or1() {
         assertThrows(IllegalArgumentException.class, () -> CprDecoder.decodePosition(0, 0, 0, 0, 2));
@@ -21,10 +30,16 @@ class CprDecoderTest {
     }
 
     @Test
-    void testDecodePosition() {
-        //todo implement properly
-        System.out.println(CprDecoder.decodePosition(111600*Math.pow(2, -17), 94445*Math.pow(2, -17), 108865*Math.pow(2, -17), 77558*Math.pow(2, -17), 0));
-        System.out.println(CprDecoder.decodePosition(111600*Math.pow(2, -17), 94445*Math.pow(2, -17), 108865*Math.pow(2, -17), 77558*Math.pow(2, -17), 1));
-
+    void testDecodePositionWithGivenValues() {
+        GeoPos[] expected = new GeoPos[]{new GeoPos((int) Units.convert(7.476062d, Units.Angle.DEGREE, Units.Angle.T32), (int) Units.convert(46.323349d, Units.Angle.DEGREE, Units.Angle.T32)), new GeoPos((int) Units.convert(7.475166d, Units.Angle.DEGREE, Units.Angle.T32), (int) Units.convert(46.322363d, Units.Angle.DEGREE, Units.Angle.T32))};
+        double epsilon = 10e-9;
+        GeoPos[] actual = new GeoPos[]{(CprDecoder.decodePosition(normalizedLong_CPR[0], normalizedLat_CPR[0], normalizedLong_CPR[1], normalizedLat_CPR[1], 0)),CprDecoder.decodePosition(normalizedLong_CPR[0], normalizedLat_CPR[0], normalizedLong_CPR[1], normalizedLat_CPR[1], 1)};
+        for (GeoPos geoPos : actual) {
+            assertNotNull(geoPos);
+        }
+        for (int i = 0; i < 2; i++) {
+            assertEquals(expected[i].longitude(), actual[i].longitude(), epsilon);
+            assertEquals(expected[i].latitude(), actual[i].latitude(), epsilon);
+        }
     }
 }
