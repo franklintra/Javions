149a294ebee9ef018b3c0bb1a05cd7e4d3d7f0a6
Franklin Tranié <franklin.tranie@gmail.com>
2023-03-30 12:02:14 +0200

    Little fix in geopos :c

--- src/ch/epfl/javions/GeoPos.java
+++ src/ch/epfl/javions/GeoPos.java
@@ -23,7 +23,7 @@ public record GeoPos(int longitudeT32, int latitudeT32) {
      * @throws IllegalArgumentException if the latitude is not valid.
      */
     public static boolean isValidLatitudeT32(int latitudeT32) throws IllegalArgumentException {
-        return (-Math.scalb(1, 30) <= latitudeT32) && (latitudeT32 <= Math.scalb(1, 30));
+        return (-Math.pow(2, 30) <= latitudeT32) && (latitudeT32 <= Math.pow(2, 30));
     }
 
     /**
