1cd141acdb5e3094db2799799392497ec8012606
Franklin Tranié <franklin.tranie@gmail.com>
2023-04-05 14:15:26 +0200

    Latest fix + added the TOKENS.java to project. Make sure to never push it again

---
+++ src/ch/epfl/cs108/TOKENS.java
@@ -0,0 +1,18 @@
+package ch.epfl.cs108;
+
+/**
+ * @author @franklintra
+ * @project Javions
+ */
+public final class TOKENS {
+    private TOKENS() {
+    } // Prevents instantiation
+
+    public static String TOKEN_1() {
+        return "";
+    }
+
+    public static String TOKEN_2() {
+        return "";
+    }
+}
