4b7f7642866d8067bd4a29b179f0c21675883ae0
Franklin Tranié <franklin.tranie@gmail.com>
2023-04-06 18:18:54 +0200

    Updated state manager and observable aircraft state

--- src/ch/epfl/javions/gui/AircraftStateManager.java
+++ src/ch/epfl/javions/gui/AircraftStateManager.java
@@ -6,6 +6,43 @@ package ch.epfl.javions.gui;/*
  * @project Javions
  */
 
-public final class AircraftStateManager {
+import ch.epfl.javions.Units;
+import ch.epfl.javions.adsb.*;
+import ch.epfl.javions.aircraft.AircraftDatabase;
+import ch.epfl.javions.aircraft.IcaoAddress;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 
+import java.util.*;
+
+public final class AircraftStateManager {
+    private final static double maxMessageAge = 60*10e9;
+    private final Map<IcaoAddress, AircraftStateAccumulator<ObservableAircraftState>> aircraftStateAccumulators;
+    private final Set<ObservableAircraftState> observableAircraftStates;
+    public AircraftStateManager(AircraftDatabase aircraftDatabase) {
+        this.aircraftStateAccumulators = new HashMap<>();
+        this.observableAircraftStates = FXCollections.observableSet(new HashSet<>());
+
+    }
+
+    public ObservableSet<ObservableAircraftState> getStates() {
+        return (ObservableSet<ObservableAircraftState>) Collections.unmodifiableSet(observableAircraftStates);
+    }
+
+    public void updateWithMessage(RawMessage message) {
+        IcaoAddress icaoAddress = message.icaoAddress();
+        aircraftStateAccumulators.computeIfAbsent(icaoAddress, k-> {
+            ObservableAircraftState observableAircraftState = new ObservableAircraftState(icaoAddress);
+            observableAircraftStates.add(observableAircraftState);
+            return new AircraftStateAccumulator<>(observableAircraftState);
+        });
+        Message actual = MessageParser.parse(message);
+        if (actual != null) {
+            aircraftStateAccumulators.get(icaoAddress).update(actual);
+        }
+    }
+
+    public void purge() {
+        observableAircraftStates.removeIf(state -> maxMessageAge < state.getLastMessageTimeStampNs() + System.nanoTime());
+    }
 }
--- src/ch/epfl/javions/gui/ObservableAircraftState.java
+++ src/ch/epfl/javions/gui/ObservableAircraftState.java
@@ -1,4 +1,4 @@
-package ch.epfl.javions.gui;/*
+package ch.epfl.javions.gui;
 /**
  * @author @franklintra (362694)
  * @author @chukla (357550)
@@ -6,96 +6,143 @@ package ch.epfl.javions.gui;/*
  */
 
 import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.adsb.AircraftStateSetter;
 import ch.epfl.javions.adsb.CallSign;
+import ch.epfl.javions.aircraft.IcaoAddress;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 
 public final class ObservableAircraftState implements AircraftStateSetter {
-    private final String icaoAddress;
-    private int category;
-    private CallSign callSign;
-    private double altitude;
-    private double velocity;
-    private double trackOrHeading;
-    private GeoPos position;
-    private long lastMessageTimeStampNs;
+    private final Property<IcaoAddress> icaoAddress = new SimpleObjectProperty<>();
+    private final IntegerProperty category = new SimpleIntegerProperty();
+    private final Property<CallSign> callSign = new SimpleObjectProperty<>();
+    private final DoubleProperty altitude = new SimpleDoubleProperty();
+    private final DoubleProperty velocity = new SimpleDoubleProperty();
+    private final DoubleProperty trackOrHeading = new SimpleDoubleProperty();
+    private final LongProperty lastMessageTimeStampNs = new SimpleLongProperty();
+    private final Property<GeoPos> position = new SimpleObjectProperty<>();
+    private final Property<ObservableList<AirbornePos>> trajectory = new SimpleObjectProperty<>(FXCollections.observableList(new ArrayList<>()));
 
-    public ObservableAircraftState(String icaoAddress, int category, CallSign callSign, double altitude, double velocity, double trackOrHeading, GeoPos position, long lastMessageTimeStampNs) {
-        this.icaoAddress = icaoAddress;
-        this.category = category;
-        this.callSign = callSign;
-        this.altitude = altitude;
-        this.velocity = velocity;
-        this.trackOrHeading = trackOrHeading;
-        this.position = position;
-        this.lastMessageTimeStampNs = lastMessageTimeStampNs;
+    public ObservableAircraftState(IcaoAddress icaoAddress) {
+        Objects.requireNonNull(icaoAddress);
+        this.icaoAddress.setValue(icaoAddress);
     }
 
-    public String getIcaoAddress() {
-        return icaoAddress;
+    public record AirbornePos(GeoPos pos, double altitude) {
+        public AirbornePos {
+            Preconditions.checkArgument(altitude >= 0);
+            Objects.requireNonNull(pos);
+        }
     }
 
-    public double getCategory() {
-        return category;
-    }
-
-    public CallSign getCallSign() {
-        return callSign;
-    }
-
-    public double getAltitude() {
-        return altitude;
-    }
-
-    public double getVelocity() {
-        return velocity;
-    }
-
-    public double getTrackOrHeading() {
-        return trackOrHeading;
-    }
-
-    public GeoPos getPosition() {
-        return position;
-    }
 
     public long getLastMessageTimeStampNs() {
-        return lastMessageTimeStampNs;
+        return lastMessageTimeStampNs.get();
+    }
+    public int getCategory() {
+        return category.get();
+    }
+    public CallSign getCallSign() {
+        return callSign.getValue();
+    }
+    public GeoPos getPosition() {
+        return position.getValue();
+    }
+    public double getAltitude() {
+        return altitude.get();
+    }
+    public double getVelocity() {
+        return velocity.get();
+    }
+    public double getTrackOrHeading() {
+        return trackOrHeading.get();
     }
 
+    /*
+     * todo: check if this class is needed
+     */
+    public ObservableList<AirbornePos> getTrajectory() {
+        return trajectory.getValue();
+    }
 
     @Override
     public void setLastMessageTimeStampNs(long timeStampNs) {
-        this.lastMessageTimeStampNs = timeStampNs;
+        this.lastMessageTimeStampNs.set(timeStampNs);
     }
 
     @Override
     public void setCategory(int category) {
-        this.category = category;
+        this.category.set(category);
     }
 
     @Override
     public void setCallSign(CallSign callSign) {
-        this.callSign = callSign;
+        this.callSign.setValue(callSign);
     }
 
     @Override
     public void setPosition(GeoPos position) {
-        this.position = position;
+        this.position.setValue(position);
     }
 
     @Override
     public void setAltitude(double altitude) {
-        this.altitude = altitude;
+        this.altitude.set(altitude);
     }
 
     @Override
     public void setVelocity(double velocity) {
-        this.velocity = velocity;
+        this.velocity.set(velocity);
     }
 
     @Override
     public void setTrackOrHeading(double trackOrHeading) {
-        this.trackOrHeading = trackOrHeading;
+        this.trackOrHeading.set(trackOrHeading);
+    }
+
+    public ReadOnlyProperty<IcaoAddress> getIcaoAddress() {
+        return icaoAddress;
+    }
+
+    public ReadOnlyIntegerProperty categoryProperty() {
+        return category;
+    }
+
+    public ReadOnlyProperty<CallSign> callSignProperty() {
+        return callSign;
+    }
+
+    public ReadOnlyDoubleProperty altitudeProperty() {
+        return altitude;
+    }
+
+    public ReadOnlyDoubleProperty velocityProperty() {
+        return velocity;
+    }
+
+    public ReadOnlyDoubleProperty trackOrHeadingProperty() {
+        return trackOrHeading;
+    }
+
+    public ReadOnlyProperty<GeoPos> positionProperty() {
+        return position;
+    }
+
+    public ReadOnlyLongProperty lastMessageTimeStampNsProperty() {
+        return lastMessageTimeStampNs;
+    }
+
+    /**
+     * todo: check if this is needed
+     */
+    public ReadOnlyProperty<ObservableList<AirbornePos>> trajectoryProperty() {
+        return trajectory;
     }
 }
