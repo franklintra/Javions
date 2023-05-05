a6c22a9c12344651e37b305d75aee1d4b307e3e3
Krish Chawla <krish.chawla@epfl.ch>
2023-05-05 17:00:25 +0200

    Final finish for ColorRamp, progress on AircraftController

--- src/ch/epfl/javions/gui/AircraftController.java
+++ src/ch/epfl/javions/gui/AircraftController.java
@@ -9,29 +9,58 @@ import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.SVGPath;
 
 import java.util.Set;
 
 public final class AircraftController {
 
-    private ObservableSet<ObservableAircraftState> states = FXCollections.observableSet();
+    private final MapParameters mapParameters;
+    private final ObservableSet<ObservableAircraftState> states;
+    private final ObjectProperty<ObservableAircraftState> selectedAircraft;
 
-    private final ObservableSet<ObservableAircraftState> unmodifiableStates = FXCollections.unmodifiableObservableSet(states);
-    private final double altitude;
-    private final AircraftRegistration registration;
-    private final double speed;
-    private final AircraftIcon icon;
-    private final ObservableList<ObservableAircraftState.AirbornePos> trajectory;
-    // TODO: 5/4/2023 might need to add ICAO address and callsign in case registration is not known 
+    private final Pane pane;
 
+    AircraftController(MapParameters mapParameters, ObservableSet<ObservableAircraftState> states, ObjectProperty<ObservableAircraftState> selectedAircraft) {
+        this.selectedAircraft = selectedAircraft;
+        this.states = states;
+        this.mapParameters = mapParameters;
+        pane = new Pane();
 
-    public AircraftController(AircraftIcon icon, AircraftStateManager states, ObservableAircraftState observableAircraftState, ObjectProperty<AircraftStateSetter> selectedAircraft) {
-        this.states = states.states();
-        this.icon = icon;
-        this.altitude = observableAircraftState.getAltitude();
-        this.registration = observableAircraftState.getRegistration();
-        this.speed = observableAircraftState.getVelocity();
-        this.trajectory = observableAircraftState.getTrajectory();
     }
+
+    public Pane pane() {
+        return pane;
+    }
+
+    private void createPane () {
+        states.addListener((SetChangeListener<ObservableAircraftState>)
+                change -> {
+            if(change.wasAdded()) {
+                createGroup(change.getElementAdded());
+            }
+                });
+
+
+    }
+    private void createGroup(ObservableAircraftState state) {
+        Group group = new Group();
+        pane.getChildren().add(group);
+        Group trajectory = new Group();
+        group.getChildren().add(trajectory);
+        Group labelIcon = new Group();
+        Group label = new Group();
+        labelIcon.getChildren().add(labelIcon);
+        SVGPath icon = new SVGPath();
+        labelIcon.getChildren().add(icon);
+
+       AircraftIcon icon1 = AircraftIcon.iconFor(state.getAircraftData().typeDesignator(), state.getAircraftData().description(), state.getCategory(), state.getAircraftData().wakeTurbulenceCategory());
+        icon1.svgPath();
+    }
+
 
 }
--- src/ch/epfl/javions/gui/ColorRamp.java
+++ src/ch/epfl/javions/gui/ColorRamp.java
@@ -2,50 +2,48 @@ package ch.epfl.javions.gui;
 
 import javafx.scene.paint.Color;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.IllegalFormatCodePointException;
+import java.util.Arrays;
 import java.util.List;
 
 public final class ColorRamp {
+    private final Color[] colours;
 
-    public static List<Color> colors = new ArrayList<>();
-
-    public ColorRamp(List<Color> colors) {
-        if (colors.size() < 2) throw new IllegalArgumentException();
-        this.colors = colors;
-
+    public ColorRamp(Color... steps) {
+        if (steps.length < 2) throw new IllegalArgumentException();
+        this.colours = steps.clone();
     }
 
     public Color at(Double value) {
         if(value <= 0) {
-            return colors.get(0);
+            return colours[0];
         } else if (value >= 1) {
-            return colors.get(colors.size() - 1);
+            return colours[colours.length - 1];
         } else {
-            double segmentSize = 1.0 / (colors.size() - 1);
+            double segmentSize = 1.0 / (colours.length - 1);
             int i = (int) (value / segmentSize);
             double fraction = (value - i * segmentSize) / segmentSize;
-            return colors.get(i).interpolate(colors.get(i + 1), fraction);
+            return colours[i].interpolate(colours[i + 1], fraction);
         }
-
     }
-    public  static  final  ColorRamp  PLASMA  =  new  ColorRamp (List.of(
+
+    public  static  final  ColorRamp  PLASMA  =  new  ColorRamp (
             Color.valueOf( "0x0d0887ff" ), Color.valueOf( "0x220690ff" ),
             Color.valueOf( "0x320597ff" ), Color.valueOf( "0x40049dff" ),
-            Color.valueOf( " 0x4e02a2ff" ), Color.valueOf( "0x5b01a5ff" ),
-            Color.valueOf( "0x6800a8ff" ), Color.valueOf( " 0x7501a8ff " ) ,
+            Color.valueOf( "0x4e02a2ff" ), Color.valueOf( "0x5b01a5ff" ),
+            Color.valueOf( "0x6800a8ff" ), Color.valueOf( "0x7501a8ff" ),
             Color.valueOf( "0x8104a7ff" ), Color.valueOf( "0x8d0ba5ff" ),
-            Color.valueOf( "0x9814a0ff"), Color.valueOf( "0xa31d9aff" ),
+            Color.valueOf( "0x9814a0ff" ), Color.valueOf( "0xa31d9aff" ),
             Color.valueOf( "0xad2693ff" ), Color.valueOf( "0xb6308bff" ),
             Color.valueOf( "0xbf3984ff" ), Color.valueOf( "0xc7427cff" ),
-            Color.valueOf ( "0xcf4c74ff" ), Color.valueOf( "0xd6556dff" ),
-            Color.valueOf( "0xdd5e66ff" ), Color.valueOf( " 0xe3685fff " ),
-            Color.valueOf( "0xe97258ff" ), Color.valueOf( "0xee7c51ff" ) ,
+            Color.valueOf( "0xcf4c74ff" ), Color.valueOf( "0xd6556dff" ),
+            Color.valueOf( "0xdd5e66ff" ), Color.valueOf( "0xe3685fff" ),
+            Color.valueOf( "0xe97258ff" ), Color.valueOf( "0xee7c51ff" ),
             Color.valueOf( "0xf3874aff" ), Color.valueOf( "0xf79243ff" ),
-            Color.valueOf("0xfa9d3bff" ), Color.valueOf( "0xfca935ff" ),
-            Color.valueOf( "0xfdb52eff" ), Color.valueOf( "0xfdc229ff " ),
+            Color.valueOf( "0xfa9d3bff" ), Color.valueOf( "0xfca935ff" ),
+            Color.valueOf( "0xfdb52eff" ), Color.valueOf( "0xfdc229ff" ),
             Color.valueOf( "0xfccf25ff" ), Color.valueOf( "0xf9dd24ff" ),
-            Color. valueOf( "0xf5eb27ff" ), Color. valueOf( "0xf0f921ff" ))
-           );
+            Color. valueOf( "0xf5eb27ff" ), Color. valueOf( "0xf0f921ff" ));
 }
 
